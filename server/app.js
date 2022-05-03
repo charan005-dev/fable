e89711a34805e627043e7309c693e59330852670
Aravindh Shiva <aravindhshiv2@gmail.com>
2022-05-02 22:00:54 -0400

    committing user creation changes

--- server/app.js
+++ server/app.js
@@ -5,8 +5,19 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", (req, res, next) => {
+  console.log("Into the app!");
+  next();
+});
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 routes(app);
 
-app.listen(3000, () => {
-  console.log("Server started at port 3000!");
+app.listen(4000, () => {
+  console.log("Server started at port 4000!");
 });
