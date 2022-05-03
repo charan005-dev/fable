57c39dc40b78ed89709cfd930263578f09c12c23
Jeshwanth Kumar <jkumar3@stevens.edu>
2022-05-03 00:41:34 -0400

    dummy commit

--- fable-new/src/components/App.js
+++ fable-new/src/components/App.js
@@ -1,39 +1,43 @@
 import { ThemeContext } from "./ThemeContext";
-import React from 'react';
-import '../App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Account from './Account';
-import Home from './Home';
-import Landing from './Landing';
-import Navigation from './Navigation';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import {AuthProvider} from '../firebase/Auth';
-import PrivateRoute from './PrivateRoute';
-import SignOutButton from './SignOut'; 
-import Splash from './Splash';
+import React, { useContext } from "react";
+import "../App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Account from "./Account";
+import Home from "./Home";
+import Landing from "./Landing";
+import Navigation from "./Navigation";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import { AuthProvider } from "../firebase/Auth";
+import PrivateRoute from "./PrivateRoute";
+import SignOutButton from "./SignOut";
+import Splash from "./Splash";
+import Theme from "./Theme";
 function App() {
   const context = useContext(ThemeContext);
   const darkMode = context.theme.darkMode;
   return (
     <AuthProvider>
       <Router>
-        <div className='container'>
-          <header className='App-header'>
-            <Navigation /> 
+        <div className="container">
+          <header className="App-header">
+            <Navigation />
           </header>
         </div>
-        <Routes>
-          <Route path='/' element={<Splash />} />
-          <Route path='/home' element={<PrivateRoute />}>
-            <Route path='/home' element={<Home />} />
-          </Route>
-          <Route path="/account" element={<PrivateRoute />}>
-            <Route path="/account" element={<Account />} />
-          </Route>
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} /> 
-        </Routes>
+        <div className={`bg ${darkMode ? "dark" : "light"}`}>
+          <Routes>
+            <Route path="/" element={<Splash />} />
+            <Route path="/home" element={<PrivateRoute />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+            <Route path="/account" element={<PrivateRoute />}>
+              <Route path="/account" element={<Account />} />
+            </Route>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+          <Theme />
+        </div>
       </Router>
     </AuthProvider>
   );
