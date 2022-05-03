ab67b746dd01b3563491cf8170d4dd512802d445
Aravindh Shiva <aravindhshiv2@gmail.com>
2022-05-02 23:51:51 -0400

    dummy commit

--- fable-new/src/components/App.js
+++ fable-new/src/components/App.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import '../App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Account from './Account';
-import Home from './Home';
-import Landing from './Landing';
-import Navigation from './Navigation';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import {AuthProvider} from '../firebase/Auth';
-import PrivateRoute from './PrivateRoute';
-import SignOutButton from './SignOut'; 
-import Splash from './Splash';
+import React from "react";
+import "../App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Account from "./Account";
+import Home from "./Home";
+import Landing from "./Landing";
+import Navigation from "./Navigation";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import { AuthProvider } from "../firebase/Auth";
+import PrivateRoute from "./PrivateRoute";
+import SignOutButton from "./SignOut";
+import Splash from "./Splash";
+
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className='container'>
-          <header className='App-header'>
-            <Navigation /> 
+        <div className="container">
+          <header className="App-header">
+            <Navigation />
           </header>
         </div>
         <Routes>
-          <Route path='/' element={<Splash />} />
-          <Route path='/home' element={<PrivateRoute />}>
-            <Route path='/home' element={<Home />} />
+          <Route path="/" element={<Splash />} />
+          <Route path="/home" element={<PrivateRoute />}>
+            <Route path="/home" element={<Home />} />
           </Route>
-          <Route path='/account' element={<PrivateRoute />}>
-            <Route path='/account' element={<Account />} />
+          <Route path="/account" element={<PrivateRoute />}>
+            <Route path="/account" element={<Account />} />
           </Route>
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} /> 
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </AuthProvider>
