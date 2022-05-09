08043e1eeff6b95358b86e169d42ec5bfa669e2f
Charan Sundar Telaganeni <52130742+Charancherry005@users.noreply.github.com>
2022-05-09 06:30:36 -0400

    publicprofile done

--- fable-new/src/components/stories/Story.js
+++ fable-new/src/components/stories/Story.js
@@ -1,8 +1,20 @@
-import { AppBar, Box, Card, CardMedia, Grid, Paper, Typography, makeStyles, CardContent } from "@material-ui/core";
+import {
+  AppBar,
+  Box,
+  Card,
+  CardMedia,
+  Grid,
+  Paper,
+  Typography,
+  makeStyles,
+  CardContent,
+} from "@material-ui/core";
 import { Stack } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import Button from "@restart/ui/esm/Button";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 const useStyles = makeStyles({
   card: {
@@ -12,7 +24,9 @@ const useStyles = makeStyles({
     marginLeft: "auto",
     marginRight: "auto",
     borderRadius: 5,
-    border: "1px solid #000",
+    border: "0px solid #000",
+    marginBottom: "10%",
+    marginTop: "7%",
   },
   media: {
     height: "300px",
@@ -20,10 +34,49 @@ const useStyles = makeStyles({
   },
   title: {
     border: " 0px #fff",
-    width: "300px",
+    width: "auto",
+    height: "auto",
+    marginTop: "1px",
+    paddingTop: "5%",
+    fontFamily: "'Encode Sans Semi Expanded', sans-serif",
+  },
+  title1: {
+    width: "auto",
+    height: "auto",
+
+    fontFamily: "'Encode Sans Semi Expanded', sans-serif",
   },
   nameBox: {
     marginRight: "1000px",
+  },
+  button: {
+    backgroundColor: "black",
+    color: "blanchedalmond",
+    width: "90%",
+    marginLeft: "10%",
+    paddingTop: "5%",
+    paddingBottom: "5%", 
+    paddingRight:"5%", 
+    paddingLeft:"5%",
+    borderRadius: "35px",
+    fontWeight: "bold",
+    fontSize: "16px",
+    textDecoration: "white",
+    "&:hover": {
+      backgroundColor: "blanchedalmond",
+      color: "black",
+      textDecoration: "white",
+      fontWeight: "bold",
+    },
+  },
+  card1: {
+    width: "70%",
+    marginLeft: "10%",
+    paddingRight: "10%",
+  },
+  card2: {
+    width: "15%",
+    marginBottom: "100px",
   },
 });
 
@@ -45,8 +98,10 @@ const Story = () => {
     console.log(storyData);
     return (
       <div>
+        <br/> 
+        <br/>
         <Paper
-          elevation={20}
+          elevation={10}
           sx={{
             bgcolor: "background.default",
             display: "grid",
@@ -55,13 +110,27 @@ const Story = () => {
           }}
         >
           <Grid container justifyContent="center">
-            <Stack direction={"row"} spacing={1}>
-              <Card className={classes.card}>
-                <CardMedia className={classes.media} component="img" image={storyData.story.coverImage} />
+            <Stack direction={"row"} spacing={7}>
+              <Card className={classes.card} elevation={15}>
+                <CardMedia
+                  className={classes.media}
+                  component="img"
+                  image={storyData.story.coverImage}
+                />
               </Card>
-              <Card className={classes.title}>
+              <Card className={classes.title} elevation={0}>
                 <CardContent>
-                  <Typography variant="h5">{storyData.story.title}</Typography>
+                  <Typography variant="h2" className={classes.title1}>
+                    {storyData.story.title}
+                  </Typography>{" "}
+                  <br />
+                  <br />
+                  <Link to="/libraries/me">
+                    <Button className={classes.button}>
+                      {" "}
+                      <MenuBookIcon /> &nbsp;&nbsp;Start Reading{" "}
+                    </Button>
+                  </Link>
                 </CardContent>
               </Card>
             </Stack>
@@ -69,15 +138,62 @@ const Story = () => {
         </Paper>
         <br />
         <br />
-        <Grid container justifyContent="center">
-          <Stack direction={"column"} spacing={4}>
-            {/* <Typography variant="h6">{storyData.creator}</Typography> */}
-            <Box className={classes.nameBox}>
-              <Link to={`/users/${storyData.creator._id}`}>{storyData.creator.displayName}</Link>
-            </Box>
-            <Typography variant="subtitle">{storyData.story.shortDescription}</Typography>
-          </Stack>
-        </Grid>
+        <Paper
+          elevation={0}
+          sx={{
+            bgcolor: "background.default",
+            display: "grid",
+            gridTemplateColumns: { md: "1fr 1fr" },
+            gap: 2,
+            height: "auto",
+          }}
+        >
+          <Grid container justifyContent="center">
+            <Stack direction={"row"} spacing={2}>
+              {/* <Typography variant="h6">{storyData.creator}</Typography> */}
+              <Card className={classes.card1} elevation={0}>
+                <CardContent>
+                  <Link to={`/users/${storyData.creator._id}`}>
+                    {storyData.creator.displayName}
+                  </Link>
+                </CardContent>{" "}
+                <br />
+                <CardContent>
+                  {" "}
+                  <Typography variant="subtitle">
+                    {storyData.story.shortDescription}
+                  </Typography>{" "}
+                </CardContent>
+              </Card>
+
+              <Card className={classes.card2} elevation={24}>
+                <CardContent>
+                  <Typography variant="subtitle">
+                    Each time I refer to one of them, I make it a point to
+                    stress to my students they are not universally true and
+                    should always be challenged. My discomfort with these 3–5–7
+                    models (as I like to call them) is not just from the
+                    absurdity of trying to distill the complexity of the world’s
+                    eternally evolving heritage of stories into single models,
+                    which invariably are littered with caveats and exceptions.
+                    No. My bigger concern is that the 3–5–7 models have largely
+                    also become foundations for exclusion of certain stories—and
+                    the focus on models is somewhat akin to the way in which
+                    global economists spend years arguing about which capitalist
+                    models work better, without giving any attention to the fact
+                    that all successful capitalist nations were built on
+                    inhumane exploitation of serfs, women, occupied lands,
+                    slaves, children—and in more recent success stories like
+                    Singapore and South Korea, underpaid labor and suppression
+                    of civil liberties. Just as it is within the world of
+                    fiction, focusing on the economic models and not the
+                    historic biases and nuances is misleading.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Stack>
+          </Grid>
+        </Paper>
       </div>
     );
   }
