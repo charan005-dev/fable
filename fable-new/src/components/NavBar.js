509fa86493cbbb631d3a545d8def2f7594fca390
Jeshwanth Kumar <jkumar3@stevens.edu>
2022-05-09 23:12:24 -0400

    support for viewLibrary component

--- fable-new/src/components/NavBar.js
+++ fable-new/src/components/NavBar.js
@@ -7,23 +7,17 @@ import IconButton from "@mui/material/IconButton";
 import Switch from "@mui/material/Switch";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
-import { styled, alpha } from '@mui/material/styles';
+import { styled, alpha } from "@mui/material/styles";
 import MenuItem from "@mui/material/MenuItem";
-import SendIcon from '@mui/icons-material/Send';
-import EditIcon from '@mui/icons-material/Edit';
-import Divider from '@mui/material/Divider';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SendIcon from "@mui/icons-material/Send";
+import EditIcon from "@mui/icons-material/Edit";
+import Divider from "@mui/material/Divider";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import Menu from "@mui/material/Menu";
-import {
-  makeStyles,
-  Button,
-  Logout,
-  ListItemIcon,
-  Image,
-} from "@material-ui/core";
+import { makeStyles, Button, Logout, ListItemIcon, Image } from "@material-ui/core";
 import { doSignOut } from "../firebase/FirebaseFunctions";
 import Tooltip from "@mui/material/Tooltip";
 import Avatar from "@mui/material/Avatar";
@@ -44,15 +38,15 @@ const useStyles = makeStyles({
     backgroundColor: "blanchedalmond",
     color: "black",
     border: "4px solid",
-  }, 
+  },
 
-  title1:{
-textDecoration:"none", 
-color: "black",
-"&:hover": {
-  textDecoration: "none", 
-  color: "black"
-},
+  title1: {
+    textDecoration: "none",
+    color: "black",
+    "&:hover": {
+      textDecoration: "none",
+      color: "black",
+    },
   },
 
   menuItem: {
@@ -88,45 +82,41 @@ color: "black",
 });
 
 const StyledMenu = styled((props) => (
-    <Menu
-      elevation={0}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      {...props}
-    />
-  ))(({ theme }) => ({
-    '& .MuiPaper-root': {
-      borderRadius: 6,
-      marginTop: theme.spacing(1),
-      minWidth: 180,
-      color:
-        theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-      boxShadow:
-        'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-      '& .MuiMenu-list': {
-        padding: '4px 0',
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color: theme.palette.mode === "light" ? "rgb(55, 65, 81)" : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
       },
-      '& .MuiMenuItem-root': {
-        '& .MuiSvgIcon-root': {
-          fontSize: 18,
-          color: theme.palette.text.secondary,
-          marginRight: theme.spacing(1.5),
-        },
-        '&:active': {
-          backgroundColor: alpha(
-            theme.palette.primary.main,
-            theme.palette.action.selectedOpacity,
-          ),
-        },
+      "&:active": {
+        backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
       },
     },
-  }));
+  },
+}));
 
 export default function NavBar() {
   const [auth, setAuth] = React.useState(true);
@@ -179,74 +169,64 @@ export default function NavBar() {
     <Box sx={{ flexGrow: 1 }}>
       <FormGroup>
         <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
           label={auth ? "Logout" : "Login"}
         />
       </FormGroup>
 
       <AppBar position="absolute" className={classes.card}>
         <Toolbar>
-  
-        <Link to='/home' className={classes.title1}>  <Typography variant="h3" component="div" sx={{ flexGrow: 0 , textDecoration: 'none'}}>
-            FABLE
-          </Typography> </Link>
+          <Link to="/home" className={classes.title1}>
+            {" "}
+            <Typography variant="h3" component="div" sx={{ flexGrow: 0, textDecoration: "none" }}>
+              FABLE
+            </Typography>{" "}
+          </Link>
           <SearchBox />
           {auth && (
             <div>
-                <Button
-                    id="demo-customized-button"
-                    aria-controls={open ? 'demo-customized-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    variant="contained"
-                    disableElevation
-                    onClick={handleClick}
-                    endIcon={<KeyboardArrowDownIcon />}
-                >
-                    Write
-                </Button>
-                <StyledMenu
-                    id="demo-customized-menu"
-                    MenuListProps={{
-                    'aria-labelledby': 'demo-customized-button',
-                    }}
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={handleClose} disableRipple>
-                    <FileCopyIcon />
-                    <Link to='/stories/create_story'>
-                        Create a New Story
-                    </Link>
-                    </MenuItem>
-                </StyledMenu>
+              <Button
+                id="demo-customized-button"
+                aria-controls={open ? "demo-customized-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                variant="contained"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+              >
+                Write
+              </Button>
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  "aria-labelledby": "demo-customized-button",
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose} disableRipple>
+                  <FileCopyIcon />
+                  <Link to="/stories/create_story">Create a New Story</Link>
+                </MenuItem>
+              </StyledMenu>
 
               <Box sx={{ flexGrow: 0 }}>
                 <Tooltip title="Open settings">
                   <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="/static/images/avatar/2.jpg"
-                    />
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                   </IconButton>
                 </Tooltip>
-                <Button
-                  component={Link}
-                  to={`/users/${currentUser.uid}/`}
-                  className={classes.button}
-                >
+                <Button component={Link} to={`/users/${currentUser.uid}/`} className={classes.button}>
                   Profile
                 </Button>
                 <Button onClick={doSignOut} className={classes.button}>
                   Logout
                 </Button>{" "}
+                <Button component={Link} to={`libraries/me`} className={classes.button}>
+                  Library
+                </Button>
               </Box>
             </div>
           )}
