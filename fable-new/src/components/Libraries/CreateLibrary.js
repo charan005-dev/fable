a731f1ad264717d32130424e8de7e2339f3e1735
Charan Sundar Telaganeni <52130742+Charancherry005@users.noreply.github.com>
2022-05-13 04:13:40 -0400

    completed create library

--- fable-new/src/components/Libraries/CreateLibrary.js
+++ fable-new/src/components/Libraries/CreateLibrary.js
@@ -1,5 +1,20 @@
-import { FormControlLabel, Grid, OutlinedInput, Paper, Select, Switch, Typography } from "@material-ui/core";
-import { Button, MenuItem, TextField, FormControl, Alert, Input } from "@mui/material";
+import {
+  FormControlLabel,
+  Grid,
+  OutlinedInput,
+  Paper,
+  Select,
+  Switch,
+  Typography,
+} from "@material-ui/core";
+import {
+  Button,
+  MenuItem,
+  TextField,
+  FormControl,
+  Alert,
+  Input,
+} from "@mui/material";
 import { makeStyles } from "@material-ui/styles";
 import React, { useState } from "react";
 import { useContext } from "react";
@@ -53,6 +68,14 @@ const useStyles = makeStyles({
       color: "blanchedalmond",
     },
   },
+  headertext1: {
+    color: "black",
+    justifyContent: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    fontSize: "30px",
+    backgroundColor: "#ecece",
+  },
 
   button3: {
     backgroundColor: "blanchedalmond",
@@ -64,22 +87,34 @@ const useStyles = makeStyles({
       color: "blanchedalmond",
     },
   },
+  paper: {
+    width: "25.3%",
+    marginLeft: "38.5%",
+    paddingLeft: "2%",
+    paddingRight: "2%",
+  },
 
   headertext: {
-    backgroundColor: "black",
-    color: "blanchedalmond",
-    width: "auto",
+    color: "black",
+    justifyContent: "center",
     marginLeft: "auto",
     marginRight: "auto",
-    paddingRight: "30px",
-    paddingLeft: "30px",
-    paddingTop: "10px",
-    paddingBottom: "10px",
-    borderRadius: "10px",
-    fontSize: "40px",
+    fontSize: "20px",
+  },
+  buttonupdate: {
+    backgroundColor: "black",
+    color: "white",
+    width: "auto",
+    marginLeft: "28.5%",
+    marginRight: "auto",
+    paddingRight: "5%",
+    paddingLeft: "5%",
+    paddingTop: "3%",
+    paddingBottom: "3%",
+    borderRadius: "6px",
     "&:hover": {
-      backgroundColor: "blanchedalmond",
-      color: "black",
+      backgroundColor: "black",
+      color: "white",
     },
   },
 });
@@ -116,55 +151,72 @@ const CreateLibrary = () => {
 
   return (
     <div>
-      {creationSuccess && <Alert severity="success">Library Creation Successful!</Alert>}
-      <Grid container justifyContent="center" alignItems="center">
-        <Typography variant="h3" component={"h1"} className={classes.headertext}>
-          Create your own library here!
-        </Typography>
-        <br />
-        <br />
-        <br />
-        <FormControl variant="standard" sx={{ m: 2, minWidth: "98.5%" }}>
-          <Typography className={classes.story}>Library Name *</Typography>
-          <br />
-          <Input
-            sx={{
-              width: "30%",
-              marginLeft: "auto",
-              marginRight: "auto",
-              paddingTop: "35px",
-              border: "4px black",
-            }}
-            id="libraryName"
-            label="Enter a name for your library"
-            variant="filled"
-            value={libraryName}
-            required
-            onChange={(e) => {
-              setLibraryName(e.target.value);
-            }}
-          />
-
-          <br />
-          <br />
-
-          <Typography className={classes.story}>
-            Want everyone to view your library? Make it public *
-            <Switch
-              checked={isPrivate}
-              onChange={() => {
-                setIsPrivate(!isPrivate);
-              }}
-              label={isPrivate ? "Public" : "Private"}
-            />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Paper className={classes.paper} elevation={24}>
+        {creationSuccess && (
+          <Alert severity="success">Library Creation Successful!</Alert>
+        )}
+        <Grid container justifyContent="center" alignItems="center">
+          <Typography
+            variant="h3"
+            component={"h4"}
+            className={classes.headertext1}
+          >
+            Create your own library here!
           </Typography>
           <br />
           <br />
-          <Button variant="contained" onClick={createLibrary} className={classes.button1}>
-            Create Library
-          </Button>
-        </FormControl>
-      </Grid>
+          <br />
+          <br />
+          <FormControl variant="standard" sx={{ m: 2, minWidth: "98.5%" }}>
+            <br />
+            <Typography variant="h3" className={classes.headertext}>
+              Library Name *
+            </Typography>
+            <br />
+            <TextField
+              fullWidth
+              className={classes.textfield}
+              id="displayName"
+              variant="outlined"
+              label=" "
+              value={libraryName}
+              InputLabelProps={{ shrink: false }}
+              onChange={(e) => {
+                setLibraryName(e.target.value);
+              }}
+            />
+
+            <br />
+            <br />
+
+            <Typography className={classes.headertext} variant={"h3"}>
+              Want everyone to view your library? Make it public *
+              <Switch
+                checked={isPrivate}
+                onChange={() => {
+                  setIsPrivate(!isPrivate);
+                }} 
+                color="primary"
+                label={isPrivate ? "Public" : "Private"}
+              />
+            </Typography>
+            <br />
+            <br />
+            <Button
+              variant="contained"
+              onClick={createLibrary}
+              className={classes.buttonupdate}
+            >
+              Create Library
+            </Button>
+            <br />
+          </FormControl>
+        </Grid>
+      </Paper>
     </div>
   );
 };
