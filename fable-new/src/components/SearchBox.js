827ebe3c4fc757ccd456d4a83205b0da2ced5bf1
Charan Sundar Telaganeni <52130742+Charancherry005@users.noreply.github.com>
2022-05-13 02:22:28 -0400

    search results done

--- fable-new/src/components/SearchBox.js
+++ fable-new/src/components/SearchBox.js
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import { makeStyles } from "@material-ui/styles";
 import axios from "axios";
 import {
   Card,
@@ -20,13 +21,64 @@ import {
   CardMedia,
   Divider,
   Tooltip,
+  Button,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../firebase/Auth";
+import { ClassNames } from "@emotion/react";
+
+const useStyles = makeStyles({
+  card: {
+    background: "black",
+    color: "white",
+    marginTop: "4%",
+    textDecoration: "none",
+    paddingBottom: "5%",
+    paddingTop: "5%",
+    paddingLeft: "2%",
+    paddingRight: "15%",
+    height: "auto",
+    width: "60%",
+    marginLeft: "20%",
+    marginRight: "20%",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+      textDecoration: "none",
+    },
+  },
+  title: {
+    marginLeft: "10%",
+  },
+  dialog: {
+    marginLeft: "33%",
+  },
+  buttonupdate: {
+    background: "black",
+    color: "white",
+    width: "auto",
+
+    textDecoration: "none",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+      textDecoration: "none",
+    },
+  },
+  link: {
+    textDecoration: "none",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+      textDecoration: "none",
+    },
+  },
+});
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
+
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   "&:hover": {
@@ -60,7 +112,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     width: "100%",
     paddingRight: "10px",
     [theme.breakpoints.up("sm")]: {
-      width: "100%",
+      width: "80%",
       "&:focus": {
         width: "20ch",
       },
@@ -69,7 +121,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 const isValidSearchTerm = (q) => {
-  if (!q || typeof q !== "string" || q.length === 0 || q.trim().length === 0) return false;
+  if (!q || typeof q !== "string" || q.length === 0 || q.trim().length === 0)
+    return false;
   return true;
 };
 
@@ -78,6 +131,7 @@ export default function SearchBox() {
   const [searchResults, setSearchResults] = useState([]);
   const [open, setOpen] = useState(false);
   const { currentUser } = useContext(AuthContext);
+  const classes = useStyles();
 
   const handleSearchInput = (e) => {
     setSearchTerm(e.target.value);
@@ -128,8 +182,8 @@ export default function SearchBox() {
         />
       </Tooltip>
       {/* <SearchResults /> */}
-      <Dialog onClose={handleDialogClose} open={open}>
-        <DialogTitle>Search Results</DialogTitle>
+      <Dialog onClose={handleDialogClose} open={open} fullWidth>
+        <DialogTitle className={classes.dialog}>Search Results</DialogTitle>
         {searchResults && searchResults.length === 0 && (
           <div>
             <Typography sx={{ p: 2 }} variant="body1">
@@ -143,18 +197,20 @@ export default function SearchBox() {
             {searchResults &&
               searchResults.map((res) => {
                 return (
-                  <Card>
-                    <CardContent>
-                      <CardActionArea>
-                        <Link onClick={handleLinkClick} to={`/stories/${res.id}`}>
-                          <CardHeader title={res.title}></CardHeader>
-                          <br />
-                        </Link>
-                      </CardActionArea>
-                    </CardContent>
-                  </Card>
+                  <div>
+                    <NavLink to={`/stories/${res.id}`} className={classes.link}>
+                      {" "}
+                      <Card className={classes.card} onClick={handleLinkClick}>
+                        {" "}
+                        {res.title.length > 35
+                          ? res.title.substring(0, 23) + "..."
+                          : res.title}{" "}
+                      </Card>
+                    </NavLink>
+                  </div>
                 );
               })}
+            <br />
           </List>
         )}
       </Dialog>
