5da0bbc7c2ac125484580439c339697a9b8f6f3b
Charan Sundar Telaganeni <52130742+Charancherry005@users.noreply.github.com>
2022-05-13 00:33:32 -0400

    made comments

--- fable-new/src/components/stories/Comments.js
+++ fable-new/src/components/stories/Comments.js
@@ -1,4 +1,12 @@
-import { Avatar, Box, Button, Divider, FormControl, TextField, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import axios from "axios";
 import React from "react";
@@ -12,14 +20,59 @@ import { AuthContext } from "../../firebase/Auth";
 const useStyles = makeStyles({
   commentsPane: {
     paddingTop: 70,
+    paddingLeft: 50,
   },
   commentsBox: {
     margin: 10,
-    height: "inherit",
-    border: "solid",
+    height: "100%",
+    paddingTop: "2vw",
+  },
+  textfield: {
+    width: "25vw",
+    marginLeft: "13.3%",
   },
   form: {
     width: "38.7vw",
+    height: "auto",
+  },
+  button: {
+    borderRadius: "14px",
+    marginLeft: "1%",
+    marginBottom: "1%",
+    marginRight: "1%",
+    marginTop: "1%",
+    backgroundColor: "#ececec",
+    color: "black", 
+    border: "1px solid",
+    "&:hover": {
+      color: "white",
+      backgroundColor: "black",
+     
+    },
+  },
+  buttonupdate: {
+    backgroundColor: "black",
+    color: "white",
+    width: "auto",
+    marginLeft: "35.5%",
+    marginRight: "auto",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  },
+  commenttext: {
+    fontWeight: "Bold",
+    fontSize: "14px",
+    fontStyle: "italic",
+    fontColor: "grey",
+  },
+  text: {
+    paddingLeft: "12.5vw", 
+    background:"#ecece"
+  },
+  text1: {
+    paddingLeft: "8.5vw",
   },
 });
 
@@ -53,7 +106,10 @@ const Comments = ({ open, handleClose, storyId }) => {
       comment.length > 250
     ) {
       setComment(comment);
-      setCommentError({ error: true, text: "Your comment text is invalid. Enter less than 250 characters." });
+      setCommentError({
+        error: true,
+        text: "Your comment text is invalid. Enter less than 250 characters.",
+      });
       return;
     }
     setComment(comment);
@@ -62,9 +118,12 @@ const Comments = ({ open, handleClose, storyId }) => {
 
   const addComment = async () => {
     if (commentError.error) {
-      toast.error("Your input is invalid for the comment. Please recheck your inputs and try again.", {
-        theme: "dark",
-      });
+      toast.error(
+        "Your input is invalid for the comment. Please recheck your inputs and try again.",
+        {
+          theme: "dark",
+        }
+      );
       return;
     }
     try {
@@ -88,45 +147,67 @@ const Comments = ({ open, handleClose, storyId }) => {
 
   return (
     <div>
-      <SidePane className={classes.commentsPane} open={open} width={40} onClose={handleClose}>
+      <SidePane
+        className={classes.commentsPane}
+        open={open}
+        width={40}
+        height={"100%"}
+        onClose={handleClose}
+      >
         <Box className={classes.commentsBox}>
-          <Typography variant="h4" component={"h2"}>
+          <Typography variant="h4" component={"h2"} className={classes.text}>
             Comments
           </Typography>
-          <Divider />
+         
           <br />
           <div>
+            <FormControl className={classes.form}>
+              <TextField
+                fullWidth
+                className={classes.textfield}
+                variant="outlined"
+                label=" "
+                placeholder="Join the discussion!"
+                onChange={(e) => handleCommentInput(e)}
+                error={commentError.error}
+                InputLabelProps={{ shrink: false }}
+                helperText={commentError.error && commentError.text}
+              />
+              <br />
+              <Button
+                variant="contained"
+                onClick={addComment}
+                className={classes.buttonupdate}
+              >
+                Post Comment
+              </Button>
+            </FormControl>
+            <br />
+            <br />
+            <br />
+
             {comments && comments.length === 0 && (
-              <Typography variant="body2">No comments yet. Be the first person to speak out.</Typography>
+              <Typography variant="body2" className={classes.text1}>
+                No comments yet. Be the first person to speak out.
+              </Typography>
             )}
+
             {comments &&
               comments.length > 0 &&
               comments.map((comment) => {
                 return (
-                  <span>
-                    <Typography variant="body2">
-                      {comment.commenterName} says: {comment.comment}
-                    </Typography>
-                  </span>
+                  <div>
+                    <Button className={classes.button}>
+                      <Typography className={classes.commenttext}>
+                        {comment.commenterName}{" "}
+                      </Typography>
+                      &nbsp;: {comment.comment}
+                    </Button>
+                  </div>
                 );
               })}
           </div>
           <br />
-          <Divider />
-          <FormControl className={classes.form}>
-            <TextField
-              variant="filled"
-              label="Comment"
-              placeholder="Join the discussion!"
-              onChange={(e) => handleCommentInput(e)}
-              error={commentError.error}
-              helperText={commentError.error && commentError.text}
-            />
-            <br />
-            <Button variant="contained" onClick={addComment}>
-              Post Comment
-            </Button>
-          </FormControl>
         </Box>
       </SidePane>
     </div>
