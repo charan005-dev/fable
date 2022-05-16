03881e0d1e9d01a60489b2ba5e3b3490ea2c001d
Charan Sundar Telaganeni <52130742+Charancherry005@users.noreply.github.com>
2022-05-15 22:06:55 -0400

    small changes

--- fable-new/src/components/NavBar.js
+++ fable-new/src/components/NavBar.js
@@ -179,7 +179,7 @@ export default function NavBar() {
               aria-label="login switch"
             />
           }
-          label={auth ? "Logout" : "Login"}
+          
         />
       </FormGroup>
 
